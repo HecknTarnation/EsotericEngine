9b1202fe867eccc1bb70bc6b4c9fc34d666ae567
Ben Ballard <coolninjagames@gmail.com>
2022-04-07 01:47:33 -0400

    Update RPGEngine.java

--- src/main/java/com/heckntarnation/rpgengine/RPGEngine.java
+++ src/main/java/com/heckntarnation/rpgengine/RPGEngine.java
@@ -1,9 +1,41 @@
 package com.heckntarnation.rpgengine;
 
+import com.heckntarnation.rpgengine.handlers.*;
+import com.heckntarnation.rpgengine.heckscript.HeckScript;
+import java.io.File;
+
 /**
  *
  * @author Ben
  */
 public class RPGEngine {
+
+    public static DialogHandler dioHandler = new DialogHandler();
+
+    /**
+     * Can be used to change handlers to custom ones, use the no parameter
+     * version to just initialize without changing. Leave any parameter blank to
+     * use its default.
+     *
+     * @param dHandler
+     */
+    public static void init(DialogHandler dHandler) {
+        dioHandler = dHandler == null ? dioHandler : dHandler;
+        init();
+    }
+
+    /**
+     * Initializes the engine.
+     */
+    public static void init() {
+        test();
+    }
+
+    //testing
+    public static void test() {
+        File file = new File("./testscript.hs");
+        HeckScript script = dioHandler.readHeckScriptFile(file);
+        dioHandler.runHeckScirpt(script);
+    }
 
 }
