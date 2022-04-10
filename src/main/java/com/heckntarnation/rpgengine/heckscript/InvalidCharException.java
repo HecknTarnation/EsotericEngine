5940dd5331e57c644938407b7d91f1fe0073f80e
Ben Ballard <coolninjagames@gmail.com>
2022-04-09 22:47:17 -0400

    Create InvalidCharException.java

---
+++ src/main/java/com/heckntarnation/rpgengine/heckscript/InvalidCharException.java
@@ -0,0 +1,13 @@
+package com.heckntarnation.rpgengine.heckscript;
+
+/**
+ *
+ * @author Ben
+ */
+public class InvalidCharException extends Exception {
+
+    public InvalidCharException(String err) {
+        super(err);
+    }
+
+}
