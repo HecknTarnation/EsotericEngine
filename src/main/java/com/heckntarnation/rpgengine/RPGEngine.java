4547c46d1698bd4ded785ca7a0f2432448db2e1b
Ben Ballard <coolninjagames@gmail.com>
2022-04-09 22:47:05 -0400

    Update RPGEngine.java

--- src/main/java/com/heckntarnation/rpgengine/RPGEngine.java
+++ src/main/java/com/heckntarnation/rpgengine/RPGEngine.java
@@ -2,7 +2,10 @@ package com.heckntarnation.rpgengine;
 
 import com.heckntarnation.rpgengine.handlers.*;
 import com.heckntarnation.rpgengine.heckscript.HeckScript;
+import com.heckntarnation.rpgengine.heckscript.InvalidCharException;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -35,7 +38,11 @@ public class RPGEngine {
     public static void test() {
         File file = new File("./testscript.hs");
         HeckScript script = heckHandler.readHeckScriptFile(file);
-        heckHandler.runHeckScirpt(script);
+        try {
+            heckHandler.runHeckScirpt(script);
+        } catch (InvalidCharException ex) {
+            Logger.getLogger(RPGEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
