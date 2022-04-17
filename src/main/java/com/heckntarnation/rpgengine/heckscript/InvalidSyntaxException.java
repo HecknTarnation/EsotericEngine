1c3a617356fbe09ab053549b768f993bdb93de50
Ben Ballard <coolninjagames@gmail.com>
2022-04-16 23:59:41 -0400

    Create InvalidSyntaxException.java

---
+++ src/main/java/com/heckntarnation/rpgengine/heckscript/InvalidSyntaxException.java
@@ -0,0 +1,13 @@
+package com.heckntarnation.rpgengine.heckscript;
+
+/**
+ *
+ * @author Ben
+ */
+public class InvalidSyntaxException extends Exception {
+
+    public InvalidSyntaxException(String err) {
+        super(err);
+    }
+
+}
